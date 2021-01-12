dc84e970c5c02b1742f0bfd6c0f66f8edd399162
Manolis <gkouem313@gmail.com>
2021-01-12 10:53:47 +0200

    Add first comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+// stateless functional component
+// always return JSX
 
 function Greeting() {
 	return <h4>this is manolis and this is my first component</h4>;
