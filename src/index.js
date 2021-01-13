744abdbb72d21a05b864623a8d70730de3b4a9d8
Manolis <gkouem313@gmail.com>
2021-01-13 17:37:30 +0200

    Childen prop

--- src/index.js
+++ src/index.js
@@ -45,12 +45,13 @@ function BookList() {
 }
 
 // destructuring props
-const Book = ({ image, title, author }) => {
+const Book = ({ image, title, author, children }) => {
   return (
     <article className="book">
       <img src={image} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      {children}
     </article>
   );
 };
