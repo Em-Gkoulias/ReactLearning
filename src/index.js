a99b152eaf3126f51c7d9b8af7662662cb116698
Manolis <gkouem313@gmail.com>
2021-01-13 16:14:45 +0200

    Destructuring props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import "./index.css";
 
 // setup vars
 const firstBook = {
-  image: "https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg",
+  image:
+    "https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg",
   title: "The Cat's revenge",
-  author: "Amelia Cat"
+  author: "Amelia Cat",
 };
 
 const secondBook = {
@@ -23,22 +24,33 @@ const secondBook = {
 function BookList() {
   return (
     <section className="bookList">
-      <Book image={ firstBook.image } title={ firstBook.title } author={ firstBook.author } />
-      <Book image={ secondBook.image } title={secondBook.title} author={ secondBook.author } />
+      <Book
+        image={firstBook.image}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos non,
+          accusamus similique dolore qui asperiores? Itaque velit voluptatem
+          quos adipisci?
+        </p>
+      </Book>
+      <Book
+        image={secondBook.image}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = (props) => {
-
-  // destructuring
-  const {image, title, author} = props;
-
+// destructuring props
+const Book = ({ image, title, author }) => {
   return (
     <article className="book">
-      <img src={ image } alt="" />
-      <h1>{ title }</h1>
-      <h4>{ author }</h4>
+      <img src={image} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
